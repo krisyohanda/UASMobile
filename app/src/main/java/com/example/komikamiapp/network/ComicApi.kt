b395f145655b56b678fad2808f33be60e390a532
Morfz <100391684+Morfz@users.noreply.github.com>
2022-06-07 12:35:42 +0800

    Create ComicApi.kt

---
+++ app/src/main/java/com/example/komikamiapp/network/ComicApi.kt
@@ -0,0 +1,56 @@
+package com.example.komikami.network
+
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.Deferred
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL = "https://manganim.herokuapp.com/manga/"
+
+val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .addCallAdapterFactory(CoroutineCallAdapterFactory())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+interface MangaApiService {
+    @GET("page/1")
+    fun getManga(): Deferred<MangaResponse>
+}
+
+interface ManhuaApiService {
+    @GET("manhua/1")
+    fun getManhua(): Deferred<ManhuaResponse>
+}
+
+interface ManhwaApiService {
+    @GET("manhwa/1")
+    fun getManhwa(): Deferred<ManhwaResponse>
+}
+
+object MangaApi {
+    val retrofitService : MangaApiService by lazy {
+        retrofit.create(MangaApiService::class.java)
+    }
+}
+
+object ManhuaApi {
+    val retrofitService : ManhuaApiService by lazy {
+        retrofit.create(ManhuaApiService::class.java)
+    }
+}
+
+object ManhwaApi {
+    val retrofitService : ManhwaApiService by lazy {
+        retrofit.create(ManhwaApiService::class.java)
+    }
+}
